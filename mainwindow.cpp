ff5ca212c8338dfdf13fc92945c9335149e216f7
Adam <adambachmann7@gmail.com>
2025-03-30 20:52:22 +0200

    First commit

---
+++ mainwindow.cpp
@@ -0,0 +1,13 @@
+#include <QtGui>
+
+#include "mainwindow.h"
+#include "finddialog.h"
+#include "gotocelldialog.h"
+#include "sortdialog.h"
+#include "spreadsheet.h"
+
+MainWindow::MainWindow()
+{
+    spreadsheet = new Spreadsheet;
+    setCentralWidget(spreadsheet);
+}
---
+++ mainwindow.h
@@ -0,0 +1,11 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+class QAction;
+class QLabel;
+class FindDialog;
+class Spreadsheet;
+
+#endif // MAINWINDOW_H
